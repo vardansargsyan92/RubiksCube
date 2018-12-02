1f55251dfed7bcce787de48e711558c385db7605
vardansargsyan92 <vardansargsyan92@gmail.com>
2018-12-02 16:47:18 +0400

    Initial commit

---
+++ src/sample/model/Move.java
@@ -0,0 +1,33 @@
+package sample.model;
+
+
+public class Move {
+    private String face;
+    private long timestamp;
+    
+    public Move(String face, long timestamp){
+        this.face=face;
+        this.timestamp=timestamp;
+    }
+    public String getFace() {
+        return face;
+    }
+
+    public void setFace(String face) {
+        this.face = face;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Move{" + "face=" + face + ", timestamp=" + timestamp + '}';
+    }
+
+}
